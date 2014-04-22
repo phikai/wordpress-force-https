8c3b1fa053bb4d7c03d3a7e6dd181a1efde158fc
Kai Armstrong <kai.armstrong@gmail.com>
2014-04-21 20:59:00 -0500

    metadata updates

--- wordpress-force-https.php
+++ wordpress-force-https.php
@@ -2,16 +2,16 @@
 
 /*
  * Plugin Name: WordPress Force HTTPS
- * Plugin URI: http://pressable.com
+ * Plugin URI: https://github.com/phikai/wordpress-force-https
  * Description: Forces all of the things to HTTPS
- * Author: A. Kai Armstrong <code@pressable.com>
+ * Author: A. Kai Armstrong
  * Author URI: http://www.kaiarmstrong.com
- * Version: 0.1
+ * Version: 0.1.1
  * */
 
-function pr_force_https () {
+function toz_force_https () {
   if ( !is_ssl() ) {
     wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301 );
   }
 }
-add_action ( 'template_redirect', 'pr_force_https', 1 );
+add_action ( 'template_redirect', 'toz_force_https', 1 );
