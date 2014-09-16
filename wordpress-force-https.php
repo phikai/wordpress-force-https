e66a03befcd10b7b1d473641ce383030ffbd7f82
Kai Armstrong <kai.armstrong@gmail.com>
2014-09-16 10:33:53 -0500

    I like it better this way

--- wordpress-force-https.php
+++ wordpress-force-https.php
@@ -12,7 +12,7 @@ Version: 0.1.2
 function toz_force_https () {
   if ( !is_ssl() ) {
     wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301 );
-    exit;
+    exit();
   }
 }
 add_action ( 'template_redirect', 'toz_force_https', 1 );
