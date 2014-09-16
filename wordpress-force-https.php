0e89b424db1a21fb18bc7ea26b1423325d67a8c6
Kai Armstrong <kai.armstrong@gmail.com>
2014-09-16 10:32:03 -0500

    Adding an "exit" cause the docs suggest it for no reason

--- wordpress-force-https.php
+++ wordpress-force-https.php
@@ -6,12 +6,13 @@ Plugin URI: https://github.com/phikai/wordpress-force-https
 Description: Forces all of the things to HTTPS
 Author: A. Kai Armstrong
 Author URI: http://www.kaiarmstrong.com
-Version: 0.1.1
+Version: 0.1.2
 */
 
 function toz_force_https () {
   if ( !is_ssl() ) {
     wp_redirect('https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'], 301 );
+    exit;
   }
 }
 add_action ( 'template_redirect', 'toz_force_https', 1 );
